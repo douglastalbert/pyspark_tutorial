8b2b9ddca88c81caccb16e6ec79c590d4d6d9914
Douglas Talbert <doug.talbert@syapse.com>
2019-07-08 16:27:28 -0700

    Through 8.5

--- Spark_DataFrames/DataFrame_Basics.py
+++ Spark_DataFrames/DataFrame_Basics.py
@@ -1,8 +1,48 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import (StructField,StringType,
+                                IntegerType,StructType)
+from pyspark.sql.functions import stddev, format_number
+
 spark = SparkSession.builder.appName('Basics').getOrCreate()
 
-df = spark.read.json('Spark_DataFrames/people.json')
+#DataFrame Basics
+
+data_schema = [StructField('age',IntegerType(),True),
+                StructField('name',StringType(),True)]
+final_struc = StructType(fields=data_schema)
+
+df = spark.read.json('Spark_DataFrames/people.json',schema=final_struc)
 df.show()
 df.printSchema()
+
 print(df.columns)
 df.describe().show()
+
+#Part Two
+
+df.select('age').show()
+print(df.head(2))
+
+#Basic Operations
+
+df1 = spark.read.csv('Spark_DataFrames/appl_stock.csv', inferSchema=True, header=True)
+df1.show()
+df1.describe().show()
+
+df1.filter(df1['Adj_Close']>100).show()
+df1.select(["Date","Volume"]).show()
+qResult = df1.filter(df1['Low']==197.16).collect()
+print(qResult)
+
+#GroupBy and Aggregate Operations
+
+df2 = spark.read.csv('Spark_DataFrames/sales_info.csv',inferSchema=True,header=True)
+df2.groupBy('Company').mean().show()
+df2.agg({'Sales':'mean'}).show()
+
+sales_std = df2.select(stddev("Sales").alias('std'))
+sales_std.show()
+sales_std.select(format_number('std',2)).show()
+
+df2.orderBy("Sales").show()
+df2.orderBy(df2["Sales"].desc()).show()
